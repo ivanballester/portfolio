5f183f2a519b572035ad94e69f9720e86639286f
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-09-09 17:16:46 +0200

    Title and Description

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,20 @@ import { useEffect, useState } from "react";
 function HomePage() {
   return (
     <div>
-      <h1 className=" text-7xl font-bold text-blue-600">Welcome</h1>
+      <div className=" border-8 p-20">
+        <div>
+          <h1 className=" text-7xl font-bold text-blue-600 text-left pb-3 pl-0">
+            Bienvenido
+          </h1>
+          <p className=" text-white text-justify w-96">
+            {" "}
+            Mi nombre es Ivan Ballester, full-stack developer viviendo en España
+            actualmente. Ahora mismo solo he trabajado en proyectos de
+            aprendizaje enfocados con React.js en el front-end y Node.js en el
+            back-end.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
