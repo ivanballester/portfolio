55f4330efce3a3f2fd74274861ac983ba67576fb
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-09-09 15:31:02 +0200

    Initial commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className=" text-7xl font-bold text-blue-600">Welcome</h1>
+    </div>
+  );
+}
+
+export default HomePage;
